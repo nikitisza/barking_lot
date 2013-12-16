72b8238aa8cf151b74837d6e9fe9aa291b702468
Niki <nikolett.tisza@gmail.com>
2013-12-16 19:08:16 +1300

    update_buttons

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $("h1").text("Hello Niki");
+    $("h1 #welcome").text("Hello Niki");
+    $("body").append("<ul id='list'><li>First item</li></ul>");
+    $("#list").append("<li>Second item</li>");
+
+    var numberOfItems = $("#holiday li").length;
+    console.log(numberOfItems);
 });
